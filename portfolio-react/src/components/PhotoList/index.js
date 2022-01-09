f5d45bef5f9e4dac989c163ccdf3f4011ea0d985
Marvin Ren <marvin.ren@yale.edu>
2022-01-09 03:52:26 -0500

    images

--- portfolio-react/src/components/PhotoList/index.js
+++ portfolio-react/src/components/PhotoList/index.js
@@ -5,33 +5,37 @@ function PhotoList() {
 
     const photos = [
         {
-            title: 'Simple-Doc',
+            title: 'Simple Doc',
+            filename: 'simple-doc',
             description: 'Manages small hospital for doctors, nurses, and patients'
         },
         {
             title: 'Eat in Peace',
+            filename: 'eat-in-peace',
             description: 'Helps users to track their diet based on thier BMI'
         },
         {
             title: 'Weather Dashboard',
+            filename: 'weather-dashboard',
             description: 'Checks weather in cities all over the world using third party API'
         },
         {
             title: 'Team Generator',
+            filename: 'team-generator',
             description: 'Generates a profile of all team members'
         },
         {
             title: 'Run Buddy',
+            filename:'run-buddy',
             description: 'a front-end html for a trainer matching business'
         }
     ]
     console.log("photoname: ", photos[0].title)
     return (
         <div className="flex-row">
-        
-            {photos.map((photo)=> {
+            {photos.map((photo)=> { 
             <img
-            src={require(`../../assets/images/${photo.title}.jpeg`)}
+            src={`../../assets/images/${photo.filename}.jpg`}
             alt={photo.title}
             className="img-thumbnail mx-1"
             key={photo.title}
--- portfolio-react/src/components/Portfolio/index.js
+++ portfolio-react/src/components/Portfolio/index.js
@@ -4,13 +4,14 @@ import PhotoList from '../PhotoList';
 
 
 function Portfolio({currentSection}) {
-    const { name, description } = currentSection
+    // const { name, description } = currentSection
     return(
         <section>
-            <h1>{name}</h1>
-            <p>{description}</p>
+            {/* <h1>{name}</h1>
+            <p>{description}</p> */}
             <PhotoList>
             </PhotoList>
+            <p>Hello</p>
         </section>
     );
 }
